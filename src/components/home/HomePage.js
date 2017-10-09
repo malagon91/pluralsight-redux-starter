a6a69275ed59c8e65517da5f5147df9de6ee9a1d
miguel malagon <miguel.malagon@mexmax-internet.com>
2017-10-09 10:01:40 -0500

    initial structur app module finished

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -7,7 +7,7 @@ class HomePage extends React.Component {
       <div className="jumbotron">
         <h1>Pluralsight Administration</h1>
         <p>React, Redux and react router in ES6 for ultra-responsive web apps.</p>
-        <Link to="about" ClassName="btn btn-primary btn-lg">Learn more</Link>
+        <Link to="about" className="btn btn-primary btn-lg" >Learn more</Link>
       </div>
     );
   }
