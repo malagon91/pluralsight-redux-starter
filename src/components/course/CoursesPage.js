7f28c33bc96a5128db9f91f502c522c17d86cc8a
miguel malagon <miguel.malagon@mexmax-internet.com>
2017-10-12 17:53:42 -0500

    last changes in async writes in redux module

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -3,10 +3,12 @@ import {connect} from 'react-redux';
 import * as courseActions from '../../actions/courseActions';
 import {bindActionCreators} from 'redux';
 import CourseList from './CourseList';
+import {browserHistory} from 'react-router';
 
 class CoursesPage extends React.Component {
   constructor (props, context){
     super(props,context);
+    this.redirectToAddCoursePage = this.redirectToAddCoursePage.bind(this);
 /*    this.state = {
       course:{ title:""}
     };
@@ -29,6 +31,10 @@ class CoursesPage extends React.Component {
     return <div key={index}>{course.title}</div>;
   }
 
+  redirectToAddCoursePage(){
+    browserHistory.push('/course');
+  }
+
 
   render(){
     const {courses} = this.props;
@@ -36,6 +42,11 @@ class CoursesPage extends React.Component {
     return (
       <div>
         <h1>Courses</h1>
+        <input
+        type="submit"
+        value="Add Course"
+        className="btn btn-primary"
+        onClick={this.redirectToAddCoursePage}/>
         <CourseList courses={courses}/>
       </div>
     );
--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -1,11 +1,18 @@
 import * as types from '../actions/actionTypes';
+import initialState from './initialState';
 
-
-export default function courseReducer(state =  [],action){
+export default function courseReducer(state =  initialState.courses,action){
   switch (action.type){
     case types.LOAD_COURSES_SUCCESS:
       return action.courses;
-
+    case types.CREATE_COURSE_SUCCESS:
+      return [...state, Object.assign({},action.course)];
+    case types.UPDATE_COURSE_SUCCESS:
+      //en este caso como el state es inmutable lo que se hace es buscar el curso que se actualizo
+      //y el metodo filter va a regresar todos menos ese curso entonces solo agregamos el actualizado
+      return [
+        ...state.filter(course => course.id !== action.course.id),Object.assign({},action.course)
+      ]
     default:
       return state;
   }
