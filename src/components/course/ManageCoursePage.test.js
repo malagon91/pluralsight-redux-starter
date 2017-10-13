9c071d358b95cf9ccef7cd9a3aa55f6f873bc8f4
miguel malagon <miguel.malagon@mexmax-internet.com>
2017-10-13 18:28:28 -0500

    change error message

--- src/components/course/ManageCoursePage.test.js
+++ src/components/course/ManageCoursePage.test.js
@@ -21,6 +21,6 @@ describe('Manage Course Page',()=>{
     const saveButton = wrapper.find('input').last();
     expect(saveButton.prop('type')).toBe('submit');
     saveButton.simulate('click');
-    expect(wrapper.state().errors.title).toBe('');
+    expect(wrapper.state().errors.title).toBe('Title must be at least 5 characters');
   });
 });
